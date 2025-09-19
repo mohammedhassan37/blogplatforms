b5b3dfe1256c228e9a08250fe2d7fa7a82e08269
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-19 03:14:24 +0100

    ArticleDisplayer structure
    Added css to articleStrucure, using inspiration from herosections.css for the main css. and also added a data folder with data.js to store all of the article information inside of it.

--- src/components/ArticleDisplayer.jsx
+++ src/components/ArticleDisplayer.jsx
@@ -3,7 +3,12 @@ import '../styles/ArticleDisplayer.css'
 
 function ArticleDisplayer(){
 return(
-    <></>
+    <>
+        <div className="ArticleDisplayer">
+            <span className='articleTop'><p id='articleTitle'>Latest Articles</p> <a href="" id='articleView'>View All</a></span>
+            
+        </div>
+    </>
 )
 }
 
