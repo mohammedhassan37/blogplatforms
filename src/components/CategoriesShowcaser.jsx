9fb2de5616811c88e71145bced575c5d2f8a12f6
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-20 07:13:13 +0100

    added two letters....
    forgot .js in file

--- src/components/CategoriesShowcaser.jsx
+++ src/components/CategoriesShowcaser.jsx
@@ -1,34 +1,51 @@
 import "../styles/CategoriesShowcaser.css";
-import { countCategories } from "../data/CategoriesData"; // imports function to count
+import { countCategories } from "../data/CategoriesData.js"; // imports function to count
 
 function CategoriesShowcaser() {
-  const { ai, mobile, websiteDev, gaming, cloud, tech } = countCategories(); 
+  const { ai, mobile, websiteDev, gaming, cloud, tech } = countCategories();
   // Calls countCategories(), then destructures the returned object so each variable
   // (ai, mobile, websiteDev, gaming, cloud, tech) gets its corresponding value.
 
   return (
     <>
-  
       <div className="catSCon ">
         <p className="catHead">Categories</p>
         <ul>
           <li>
-            <span><a href="">Artificial Intelligence & Machine Learning</a><p id="articleAmounts">{ai}</p></span>
+            <span>
+              <a href="">Artificial Intelligence & Machine Learning</a>
+              <p id="articleAmounts">{ai}</p>
+            </span>
           </li>
           <li>
-            <span><a href="">Cloud</a><p id="articleAmounts">{cloud}</p></span>
+            <span>
+              <a href="">Cloud</a>
+              <p id="articleAmounts">{cloud}</p>
+            </span>
           </li>
           <li>
-            <span><a href="">Mobile</a><p id="articleAmounts">{mobile}</p></span>
+            <span>
+              <a href="">Mobile</a>
+              <p id="articleAmounts">{mobile}</p>
+            </span>
           </li>
           <li>
-            <span><a href="">Website Development</a><p id="articleAmounts">{websiteDev}</p></span>
+            <span>
+              <a href="">Website Development</a>
+              <p id="articleAmounts">{websiteDev}</p>
+            </span>
           </li>
           <li>
-            <span><a href="">Gaming</a><p id="articleAmounts">{gaming}</p></span>
+            <span>
+              <a href="">Gaming</a>
+              <p id="articleAmounts">{gaming}</p>
+            </span>
           </li>
           <li>
-            <span><a href="">Technology</a><p id="articleAmounts">{tech}</p></span>
+            <span>
+              <a href="">Technology</a>
+              <p id="articleAmounts">{tech}</p>
+            </span>
           </li>
         </ul>
       </div>
