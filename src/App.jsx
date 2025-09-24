bd075116e273d9c0c2c0810084f6739fb629e463
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-24 17:23:58 +0100

    Add React Router and Meow page, update navigation
    Integrated react-router-dom for client-side routing. Added a new Meow page and updated Header navigation to use Link components. Refactored App.jsx to use Routes and Route for page rendering. Applied a dark background style in index.css and updated main.jsx to wrap App in BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import Header from './components/Header.jsx'
-import HeroSection from './components/HeroSection.jsx'
-import ArticleShowcaser from './components/ArticleShowcaser.jsx'
-import CategoriesShowcaser from './components/CategoriesShowcaser.jsx'
-import PoularArticls from './components/PopularArticles.jsx'
+import { Routes, Route } from "react-router-dom";
+import Header from './components/Header.jsx';
+import HeroSection from './components/HeroSection.jsx';
+import ArticleShowcaser from './components/ArticleShowcaser.jsx';
+import CategoriesShowcaser from './components/CategoriesShowcaser.jsx';
+import PoularArticls from './components/PopularArticles.jsx';
+import Meow from './Pages/Meow.jsx';
+import './index.css';
 
 function App() {
- 
-
   return (
     <>
-      <Header></Header>
-      <HeroSection></HeroSection>
-      <ArticleShowcaser></ArticleShowcaser>
-      <CategoriesShowcaser></CategoriesShowcaser>
-      <PoularArticls></PoularArticls>
+      <Header />
+
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <ArticleShowcaser />
+              <CategoriesShowcaser />
+              <PoularArticls />
+            </>
+          }
+        />
+
+        {/* Meow Page */}
+        <Route path="/meow" element={<Meow />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
