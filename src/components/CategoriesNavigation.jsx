190ffd434bb38d3bcc4fa6fbaaf9792a3ff939f8
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-30 14:51:31 +0100

    CategoriesNav
    Added IDs to the li elements in catnav.jsx to ensure that those specific elements can get specific colour and etc. also added a new way to categorise my css to ensure its understandable and structured properly.

--- src/components/CategoriesNavigation.jsx
+++ src/components/CategoriesNavigation.jsx
@@ -9,29 +9,29 @@ return(
             <p id="CatNavText">Click on any of these buttons to see the different pages I've created for the BlogPost platform:</p>
                 <div className="CatNavBtns">
                     <ul>
-                        <li>
-                            <Link to="/AI">🤖 AI & ML</Link>
+                        <li id="CatNavAI">
+                            <Link to="/AI" >🤖 AI & ML</Link>
                         </li>
-                        <li>
-                            <Link to="/Mobile">📱 Mobile</Link>
+                        <li id="CatNavMobile">
+                            <Link to="/Mobile" >📱 Mobile</Link>
                         </li>
-                        <li>
+                        <li id="CatNavWebDev">
                             <Link to="/AI">💻 Web Dev</Link>
                         </li>
-                        <li>
+                        <li id="CatNavBlockChain">
                             <Link to="/AI">⛓️ Blockchain</Link>
                         </li>
-                        <li>
-                            <Link to="/AI">🕹️ Gaming</Link>
+                        <li id="CatNavGaming">
+                            <Link to="/AI" >🕹️ Gaming</Link>
                         </li>
-                        <li>
-                            <Link to="/AI">⚙️ Technology</Link>
+                        <li id="CatNavTech">
+                            <Link to="/AI" >⚙️ Technology</Link>
                         </li>
-                        <li>
-                            <Link to="/AI">⭐ Reviews</Link>
+                        <li id="CatNavReviews">
+                            <Link to="/AI" >⭐ Reviews</Link>
                         </li>
-                        <li>
-                            <Link to="/AI">📧 Contact</Link>
+                        <li id="CatNavContact">
+                            <Link to="/AI" >📧 Contact</Link>
                         </li>
                     </ul>
                 </div>
