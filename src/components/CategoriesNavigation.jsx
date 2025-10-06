174e11f937649cb3e0ee438e440b5c64fcee1962
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-06 21:47:08 +0100

    a
    a

--- src/components/CategoriesNavigation.jsx
+++ src/components/CategoriesNavigation.jsx
@@ -4,46 +4,9 @@ import { Link } from "react-router-dom";
 function CategoriesNavigation() {
   return (
     <>
-      <div className="CatNavConCon">
-        <div className="CatNavCon">
-          <div className="CatNavText">
-            <h5 id="CatNavHead">🚀 Explore all pages</h5>
-            <h1>Discover All BlogPlatform Pages</h1>
-            <p id>
-              Navigate through our comprehensive technology platform with
-              specialized sections for every tech enthusiast
-            </p>
-          </div>
-          <div className="CatNavBtns">
-            <ul>
-              <Link to="/AI">
-                <li id="CatNavAI">🤖 AI & ML</li>
-              </Link>
-              <Link to="/Mobile">
-                <li id="CatNavMobile">📱 Mobile</li>
-              </Link>
-              <Link to="/WebDev">
-                <li id="CatNavWebDev">💻 Web Dev</li>
-              </Link>
-              <Link to="/Blockchain">
-                <li id="CatNavBlockChain">⛓️ Blockchain</li>
-              </Link>
-              <Link to="/Gaming">
-                <li id="CatNavGaming">🕹️ Gaming</li>
-              </Link>
-              <Link to="/Technology">
-                <li id="CatNavTech">⚙️ Technology</li>
-              </Link>
-              <Link to="/Reviews">
-                <li id="CatNavReviews">⭐ Reviews</li>
-              </Link>
-              <Link to="/Contact">
-                <li id="CatNavContact">📧 Contact</li>
-              </Link>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <div className="CatNavMain">
+          .  
+      </div> 
     </>
   );
 }
